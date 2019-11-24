e79c841ffef49f80d7d5c1f87462b65f31b688ce
baranov.ll <lev.baranov@domru.ru>
2019-11-24 15:02:56 +0500

    modified:   test_es.py

--- test_es.py
+++ test_es.py
@@ -16,34 +16,16 @@ def connect_elastic():
 	else:
 		print('Could not connect!')
 	return _es
-def search(es_object, index_name, search, size):
+def search(es_object, index_name, search, size = 1):
 	#Процедура поиска
 	res = es_object.search(index=index_name, body=search, size=size)
 	return res
 
-"""
 if __name__ == '__main__':
 	logging.basicConfig(level=logging.ERROR)
 	es = connect_elastic()
 	if es is not None:
-		index = 'steps'
-		size = 9999
-		search_body = {'query': { 'match': { 'step_name': { 'query': 'Отправить администраторам активного оборудования'}}}}
-		results = search(es, index, json.dumps(search_body), size)
-		diction = []
-		for hit in results['hits']['hits']:
-			source = hit['_source']
-			info = source['info']
-			print(info)
-			diction.extend(clsf.get_dict(info))
-		#res = result[_source]
-	diction = clsf.sort_dict(diction)
-	print(diction)
-"""
-if __name__ == '__main__':
-	logging.basicConfig(level=logging.ERROR)
-	es = connect_elastic()
-	if es is not None:
+		data_dict = {}
 		index = 'tickets'
 		size = 9999
 		search_body = {"query": {"exists":{"field":"category"}}}
@@ -52,4 +34,45 @@ if __name__ == '__main__':
 		for hit in results['hits']['hits']:
 			_id = hit['_id']
 			ids.append(_id)
-		random.choice(ids)
+		for number in range(20):
+			t_id = random.choice(ids)
+			ticket = es.get(index = 'tickets', id = t_id)
+			category = ticket['_source']['category']
+			search_body = {
+"query": {
+    "bool": {
+      "must": [
+        {
+          "match": {
+            "ticket_id": t_id
+          }
+        },
+        {
+          "match": {
+            "number": 0
+          }
+        },
+        {
+          "match": {
+            "step_name": "Отправить администраторам активного оборудования"
+          }
+        }
+      ]
+    }
+  }
+}
+			step = search(es, 'steps', json.dumps(search_body))
+			print(t_id)
+			total = step['hits']['total']
+			if total == 1:
+				info = step['hits']['hits'][0]['_source']['info']
+				print(info)
+				print(category)
+				if data_dict.get(category) is not None:
+					data_dict[category].append(info)
+				else:
+					data_dict[category] = [info]
+		print(data_dict)
+		my_file = open("data.json", "w")
+		my_file.write(json.dumps(data_dict, ensure_ascii=False))
+		my_file.close()
