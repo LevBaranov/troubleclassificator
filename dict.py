1fd7b8450957171ab9b2a4a3ef50ea2937e0b725
baranov.ll <lev.baranov@domru.ru>
2019-11-24 22:26:45 +0500

    modified:   dict.py

--- dict.py
+++ dict.py
@@ -2,7 +2,25 @@
 import json
 import numpy as np
 import random
-import tensorflow as tf
+#from pybrain.tools.shortcuts import buildNetwork
+#from pybrain.datasets import SupervisedDataSet
+#from pybrain.supervised.trainers import BackpropTrainer
+#from pybrain.tools.customxml import NetworkWriter
+
+def get_tf_record(sentence):
+    global words
+    # tokenize the pattern
+    sentence_words = sentence
+    # stem each word
+    sentence_words = [word.lower() for word in sentence_words]
+    # bag of words
+    bow = [0]*len(words)
+    for s in sentence_words:
+        for i, w in enumerate(words):
+            if w == s:
+                bow[i] = 1
+    return(np.array(bow))
+
 # read the json file and load the training data
 with open('data.json') as json_data:
     data = json.load(json_data)
@@ -14,10 +32,6 @@ words = []
 docs = []
 for each_category in data.keys():
     for each_sentence in data[each_category]:
-        # extract words from each sentence and append to the word list
-        #w = nltk.word_tokenize(each_sentence)
-        #print("tokenized words: ", w)
-        #words.(each_sentence)
         for w in each_sentence.split(' '):
             words.append(w)
         docs.append(([w for w in each_sentence.split(' ')], each_category))
@@ -53,4 +67,23 @@ training = np.array(training)
 # trainX contains the Bag of words and train_y contains the label/ category
 train_x = list(training[:, 0])
 train_y = list(training[:, 1])
+'''
+net = buildNetwork(len(train_x[0]), 1, len(train_y[0]))
+ds = SupervisedDataSet(len(train_x[0]), len(train_y[0]))
+i = 0
+for inp in train_x:
+    ds.addSample(inp, train_y[i])
+    i += 1
+
+trainer = BackpropTrainer(net, ds, learningrate=0.05)
+trainer.trainEpochs(1000) 
+
+NetworkWriter.writeToFile(net, 'net.xml')
+#net = NetworkReader.readFrom('filename.xml')
+inp = get_tf_record('УК порт 4  Нет связи, линк м МАС приходит, оборудование перезагрузили, не помогло. по логам нестабильный линк.')
+print(inp)
+out = net.activate(inp)
+print(categories[np.argmax(out)])
+print(out)
+'''
 
