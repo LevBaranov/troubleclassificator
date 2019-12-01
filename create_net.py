f713995d73523c197ec069bba216be1b6a6b448c
baranov.ll <lev.baranov@domru.ru>
2019-12-01 17:38:07 +0500

    modified:   create_net.py 	modified:   net.xml

--- create_net.py
+++ create_net.py
@@ -9,7 +9,7 @@ from pybrain.supervised.trainers import BackpropTrainer
 from pybrain.tools.customxml import NetworkWriter
 from pybrain.structure import TanhLayer, SoftmaxLayer, LinearLayer
 
-net = buildNetwork(len(dict.train_x[0]), 5, len(dict.train_y[0]), hiddenclass=LinearLayer, recurrent=True)
+net = buildNetwork(len(dict.train_x[0]), 5, len(dict.train_y[0]), hiddenclass=LinearLayer, recurrent=False)
 ds = SupervisedDataSet(len(dict.train_x[0]), len(dict.train_y[0]))
 i = 0
 for inp in dict.train_x:
